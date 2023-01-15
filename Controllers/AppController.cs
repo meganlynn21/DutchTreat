e8984234f1c9cb49bf3257f1cc6739dbd0aede5f
meganlynn21 <meganlkeyes0203@gmail.com>
2023-01-14 21:16:35 -0500

    making contact form

---
+++ Controllers/AppController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View(); 
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            return View();
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
