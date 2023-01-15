36fea713e474f1fc3bc7bdbb8bc67f493e5ce86a
meganlynn21 <meganlkeyes0203@gmail.com>
2023-01-14 23:33:45 -0500

    Added bootstrap

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DutchTreat.Services;
 using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +11,11 @@ namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+        public AppController(IMailService mailService)
+        {
+            _mailService= mailService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -24,6 +30,17 @@ namespace DutchTreat.Controllers
         [HttpPost("contact")]
         public IActionResult Contact(ContactViewModel model)
         {
+            if(ModelState.IsValid)
+            {
+                //Send the email
+                _mailService.SendMessage("rosettastone0203@gmail.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            else
+            {
+                // Show the errors
+            }
             return View();
         }
 
